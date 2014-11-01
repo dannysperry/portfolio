fd818452e14a3cf3cd0402a82338f7583121eae9
dannysperry <danny.sperry@gmail.com>
2014-10-31 23:54:39 -0700

    set up sass and compass as well as sticky footer to layout

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source :rubygems
 gem 'sinatra'
 gem 'slim'
+gem 'sass'
+gem 'compass'
 
--- portfolio.rb
+++ portfolio.rb
@@ -1,5 +1,22 @@
+require 'rubygems'
+require 'compass'
 require 'sinatra'
 require 'slim'
+
+# set sinatra's variables
+set :app_file, __FILE__
+set :root, File.dirname(__FILE__)
+set :views, "views"
+set :public_folder, 'assets'
+
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+end
+
+get '/stylesheets/:name' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :"stylesheets/#{params[:name]}", Compass.sass_engine_options
+end
 
 get '/' do
   slim :index
