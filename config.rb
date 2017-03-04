2791e6d8456c8e13ca2a293070103d0aadde853d
Daniel Sperry <danny.sperry@gmail.com>
2017-03-03 18:10:44 -0800

    Cleaned up styles for mobile and added compass init files

---
+++ config.rb
@@ -0,0 +1,24 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "assets/stylesheets"
+http_stylesheets_path = "/stylesheets"
+sass_dir = "assets/stylesheets"
+images_dir = "assets/images"
+http_images_path = "/images"
+javascripts_dir = "javascripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+environment = :production
+
+relative_assets = true
+
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass assets/stylesheets scss && rm -rf sass && mv scss sass
